2af822ce297f004225e94863a7b45db7ba1444ee
GoodDeiDie <gooddeidie@gmail.com>
2022-02-21 22:47:55 +0600

    two commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import random 
+
+pwd_lenght = int(input("Введите длину пароля:"))
+pwd_digits = input('Включить числа? ( да = y , нет = n)')
+pwd_upercase = input('Включить верхний регистр? ( да = y , нет = n)')
+pwd_lowercase = input('Включить нижний регистр? ( да = y , нет = n)')
+pwd_punctuation = input('Включить знаки "!#$%&*+-=?@^_"? ( да = y , нет = n)')
