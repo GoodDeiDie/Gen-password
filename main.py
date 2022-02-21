fafba01a921032196721be7d3d0df1b655044665
GoodDeiDie <gooddeidie@gmail.com>
2022-02-21 23:08:35 +0600

    final commit

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-import random 
+import random
+
 
 pwd_lenght = int(input("Введите длину пароля:"))
 pwd_digits = input('Включить числа? ( да = y , нет = n)')
 pwd_upercase = input('Включить верхний регистр? ( да = y , нет = n)')
 pwd_lowercase = input('Включить нижний регистр? ( да = y , нет = n)')
 pwd_punctuation = input('Включить знаки "!#$%&*+-=?@^_"? ( да = y , нет = n)')
+
+
+digits = '1234567890'
+upercase = 'ABCDEFGHIGKLMNOPQRSTUVWXYZ'
+lowercase = 'abcdefghigklmnopqrstuvwxyz'
+punctuation = '!#$%&*+-=?@^_'
+
+new_password = ''
+
+pull = ''
+
+if pwd_digits == 'y':
+    pull += digits
+if pwd_upercase == 'y':
+    pull += upercase
+if pwd_lowercase == 'y':
+    pull+= lowercase
+if pwd_punctuation == 'y':
+    pull+= punctuation
+
+for i in range(pwd_lenght):
+    new_password += pull[random.randint(0, len(pull)-1)]
+
+print("Ваш новый пароль:" + new_password)
